c7f401d1aa81855db080cdd92e35a36e8219d991
Georgi Boiko <georgi@pandasauce.org>
2020-03-06 19:53:11 +0000

    version tag change

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -34,7 +34,7 @@ extern "C" {
 # ifdef OPENSSL_FIPS
 #  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2-chacha (1.0.2k-fips-dev)"
 # else
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2-chacha (1.0.2k-dev)"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2-chacha (1.0.2k-pandasauce)"
 # endif
 # define OPENSSL_VERSION_PTEXT   " part of " OPENSSL_VERSION_TEXT
 
